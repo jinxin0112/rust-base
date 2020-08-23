baf12f8ed8943d02da0957acaedf37a7c27214d1
jinxin0112 <m18508218948@163.com>
2020-08-23 15:12:15 +0800

    init

---
+++ guess_game/src/main.rs
@@ -0,0 +1,27 @@
+use rand::{thread_rng, Rng};
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("欢迎来到 guess game!");
+
+    let secret_number = thread_rng().gen_range(1, 101);
+    // println!("随机数字是 {}", secret_number);
+
+    loop {
+        println!("请输入你数字:");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("read_line fail!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!"); // 字符串转数字
+        println!("你输入的数字是 {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small!"),
+            Ordering::Greater => println!("too big!"),
+            Ordering::Equal => {
+                println!("you win!");
+                break;
+            }
+        }
+    }
+}
